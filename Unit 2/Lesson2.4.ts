cce67dcf13350329388af15304e18ee3a8bb78ef
Błażej Domagała <petitoff@outlook.com>
2022-03-27 17:40:05 +0200

    Operators

---
+++ Unit 2/Lesson2.4.ts
@@ -0,0 +1,31 @@
+console.log(1 + 1);
+console.log(3 - 1);
+console.log(3 * 2);
+console.log(10 / 2);
+console.log(10 % 3);
+
+var a = 1;
+console.log(a++);
+console.log(a--);
+console.log(a);
+
+console.log(1 > 2);
+console.log(1 < 2);
+console.log(1 >= 2);
+console.log(1 <= 2);
+console.log(2 == 2);
+console.log(2 != 2);
+
+console.log(true && false);
+console.log(true || false);
+console.log(!true);
+
+var a = 1;
+a += 2;
+console.log(a);
+a -= 2;
+console.log(a);
+a *= 2;
+console.log(a);
+a /= 2;
+console.log(a);
